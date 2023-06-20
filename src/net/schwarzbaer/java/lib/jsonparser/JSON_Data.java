c87af97468658ef602f0719be632c07c4754e559
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-06-20 23:47:55 +0200

    test commit

--- src/net/schwarzbaer/java/lib/jsonparser/JSON_Data.java
+++ src/net/schwarzbaer/java/lib/jsonparser/JSON_Data.java
@@ -13,6 +13,7 @@ import net.schwarzbaer.java.lib.jsonparser.JSON_Data.Value.Type;
 
 public class JSON_Data {
 	
+	// test
 	public static class TraverseException extends Exception {
 		private static final long serialVersionUID = -2729201690665554532L;
 		public TraverseException(String message) {
